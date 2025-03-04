d83e746b6561ca9ee6a9f5c0cce7867ef32f8a29
hoonjo123 <johoon030@gmail.com>
2025-03-04 16:16:13 +0900

    change

--- input/index.js
+++ input/index.js
@@ -3,3 +3,8 @@ const inputElement = document.getElementById('input');
 inputElement.addEventListener('mouseover', () => {
   console.log(inputElement.value);
 });
+
+const selectElement = document.getElementById('color');
+selectElement.addEventListener('change', () => {
+  console.log(selectElement.value);
+});
